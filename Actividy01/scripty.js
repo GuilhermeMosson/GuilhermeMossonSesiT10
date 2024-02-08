825d25290c0e6d27ec2645b71ce17ae12f9d6f39
GuilhermeMosson <guimosson@gmail.com>
2024-02-07 21:49:26 -0300

    Atualização oficial desta pasta

---
+++ Actividy01/scripty.js
@@ -0,0 +1,42 @@
+function somar(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("numSoma01").valueAsNumber;
+    var num2 =  document.getElementById("numSoma02").valueAsNumber;
+    var resu = num1 + num2;
+    document.getElementById("resultadoSoma").textContent=resu;
+    
+}
+
+function Subtracao(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("numSub01").valueAsNumber;
+    var num2 =  document.getElementById("numSub02").valueAsNumber;
+    var resu = num1 - num2;
+    document.getElementById("resultadoSub").textContent=resu;
+    
+}
+
+function mult(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("numMul01").valueAsNumber;
+    var num2 =  document.getElementById("numMul02").valueAsNumber;
+    var resu = num1 * num2;
+    document.getElementById("resultadoMulti").textContent=resu;
+    
+}
+
+function divisao(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("numDiv01").valueAsNumber;
+    var num2 =  document.getElementById("numDiv02").valueAsNumber;
+    var resu = num1 / num2;
+    document.getElementById("resultadoDivi").textContent=resu;
+}
+
+function media(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("numMed01").valueAsNumber;
+    var num2 =  document.getElementById("numMed02").valueAsNumber;
+    var resu = (num1 + num2) / 2;
+    document.getElementById("resultadoMed").textContent=resu;
+}
